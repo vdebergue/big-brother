43c97b647dfbd5ed12193f195b4c54b606a9e6c5
Tewfik Sadaoui <tsadaoui@work4labs.com>
2012-12-20 17:15:58 +0100

    Base Student-School-Company models

--- student/models.py
+++ student/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    """
+    """
+    facebook_id = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=200)
+
+
+class Subject(models.Model):
+    """
+    """
+    name = models.CharField(max_length=200)
+
+
+class Student(models.Model):
+    """
+    """
+    facebook_id = models.CharField(primary_key=True, max_length=20)
+    first_name = models.CharField(max_length=200)
+    sur_name = models.CharField(max_length=200)
+    school = models.ForeignKey(School)
+    experiences = models.ForeignKey(Experience)
+    projects = models.ManyToManyField(Project)
+
+
+class Project(nodels.Model):
+    """
+    Students' school project.
+    """
+    name = models.CharField(max_length=200)
+
+
+class Experience(nodels.Model):
+    """
+    Student's previous experiences (internship, summer jobs ...)
+    """
+    name = models.CharField(max_length=200)
+    date_start = models.DateField()
+    date_end = models.DateField()
+
+
+class Company(models.Model):
+    """
+    Companies which are looking for applicants
+    """
+    facebook_id = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=200)
+
+
+class Internship(models.Model):
+    """
+    Internship posted by companies.
+    """
+    name = models.CharField(max_length=200)
+    company = models.ForeignKey(Company)
+    applicants = models.ManyToManyField(Student)
