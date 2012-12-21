44b5c2a62bd9e48af9f03d48b5791cdced2f6bac
Tewfik Sadaoui <tsadaoui@work4labs.com>
2012-12-21 05:04:49 +0100

    Set up admin for several models

--- student/admin.py
+++ student/admin.py
@@ -1,4 +1,10 @@
-from student.models import Student
+from student.models import *
 from django.contrib import admin
 
 admin.site.register(Student)
+admin.site.register(Company)
+admin.site.register(Subject)
+admin.site.register(Internship)
+admin.site.register(School)
+admin.site.register(Project)
+admin.site.register(Experience)
